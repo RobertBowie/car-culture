4b9cfda2e4a0d4f9212cfae3771aeca4bca2c4ea
RobertBowie <robertb1903@gmail.com>
2015-11-11 15:36:32 -0800

    Added route and page for event profile page.

--- car-culture/routes/index.js
+++ car-culture/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/eventprofile', function(req, res, next) {
+  res.render('eventprofile', {title: 'Vehicle Event Profile'});
+});
+
 module.exports = router;
