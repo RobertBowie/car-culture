9f46f23ebe91e073c74c1e592532479d6ec0f40e
RobertBowie <robertb1903@gmail.com>
2015-11-12 10:00:16 -0800

    Added comments to illustrate intent of routes.

--- car-culture/routes/index.js
+++ car-culture/routes/index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
+// Link up our mock database for MVP
 var db = require('../data/eventData');
 
-/* GET home page. */
+/* GET eventprofile page for MVP */
+/* Change to login/signup later  */
 router.get('/', function(req, res, next) {
   res.render('eventprofile', {title: 'Vehicle Event Profile'});
   // res.render('index', { title: 'Express' }); // Change this back later if needed.
 });
 
 router.get('/eventData', function(req, res, next) {
+  // Make a call to the async db function
   db(null, function(data) {
     res.send(data);
   });
