f012d4727f9b3b0edfc58e7f7e794d39184a7a4e
RobertBowie <robertb1903@gmail.com>
2015-11-11 23:34:19 -0800

    Added working GET from client to server DB for car data object.

--- car-culture/data/eventData.js
+++ car-culture/data/eventData.js
@@ -1,15 +1,15 @@
-var dbSim = function(options) {
-  return setTimeout(function() {
-    return {
-  "userID1": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "wagon", "mileage": 102053, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
-  "userID2": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "sedan", "mileage": 55000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
-  "userID3": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "wagon", "mileage": 75000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
-  "userID4": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "sedan", "mileage": 105000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
-  "userID5": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "sedan", "mileage": 110000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
-  "userID6": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "wagon", "mileage": 92050, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
-  "userID7": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "wagon", "mileage": 107000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
-  "userID8": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "sedan", "mileage": 108000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}}
-}
+var dbSim = function(options, cb) {
+  setTimeout(function() {
+    cb( {
+      "userID1": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "wagon", "mileage": 102053, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
+      "userID2": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "sedan", "mileage": 55000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
+      "userID3": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "wagon", "mileage": 75000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
+      "userID4": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "sedan", "mileage": 105000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
+      "userID5": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "sedan", "mileage": 110000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
+      "userID6": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "wagon", "mileage": 92050, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
+      "userID7": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "wagon", "mileage": 107000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}},
+      "userID8": {"year": 2002, "make": "Subaru", "model": "Impreza", "trim": "WRX", "body-type": "sedan", "mileage": 108000, "mods": ["cai", "exhaust", "intercooler"], "events": {"22500": "drivetrain issue", "25500": "window issue", "55500": "wheel issue", "105000": "consider a nested obj for issue storage"}}
+    } );
   }, 200);
 };
 
--- car-culture/routes/index.js
+++ car-culture/routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../data/eventData');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('eventprofile', {title: 'Vehicle Event Profile'});
   // res.render('index', { title: 'Express' }); // Change this back later if needed.
+});
+
+router.get('/eventData', function(req, res, next) {
+  db(null, function(data) {
+    res.send(data);
+  });
 });
 
 router.get('/eventprofile', function(req, res, next) {
